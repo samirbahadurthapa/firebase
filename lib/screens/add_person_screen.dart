b68224fe776c90c761dc9c5c36129b7b046e2bb4
samirbahadurthapa <sinkythapa@gmail.com>
2025-06-25 19:41:07 +0545

    finishe Ui of Form Fillup

--- lib/screens/add_person_screen.dart
+++ lib/screens/add_person_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class AddPersonScreen extends StatelessWidget {
@@ -6,9 +8,213 @@ class AddPersonScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar( 
-        title: Text("Firebase"),
-        backgroundColor: Colors.indigo,
+      appBar: AppBar(toolbarHeight: 1),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              decoration: BoxDecoration(
+                image: DecorationImage(image: AssetImage("assets/hello.png")),
+              ),
+            ),
+            SizedBox(height: 30),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Forum Fillup",
+                    style: TextStyle(
+                      color:Colors.grey,
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    "Enter your Details to Continue",
+                    style: TextStyle(color: Colors.grey[700], fontSize: 17),
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(5),
+                              borderSide: BorderSide(
+                                width: 0,
+                                color:Colors.grey,
+                              ),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide(
+                                width: 0,
+                                color:Colors.grey,
+                              ),
+                            ),
+                            contentPadding: EdgeInsets.only(top: 12, left: 20),
+                            filled: true,
+                            fillColor: Colors.white24,
+                            border: InputBorder.none,
+
+                            hintText: "Enter your first Name",
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(5),
+                              borderSide: BorderSide(
+                                width: 0,
+                                color:Colors.grey,
+                              ),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide(
+                                width: 0,
+                                color:Colors.grey,
+                              ),
+                            ),
+                            contentPadding: EdgeInsets.only(top: 12, left: 20),
+                            filled: true,
+                            fillColor: Colors.white24,
+                            border: InputBorder.none,
+
+                            hintText: "Enter your Middle Name Optional",
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(5),
+                              borderSide: BorderSide(
+                                width: 0,
+                                color:Colors.grey,
+                              ),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide(
+                                width: 0,
+                                color:Colors.grey,
+                              ),
+                            ),
+                            contentPadding: EdgeInsets.only(top: 12, left: 20),
+                            filled: true,
+                            fillColor: Colors.white24,
+                            border: InputBorder.none,
+
+                            hintText: "Enter your Last Name",
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(5),
+                              borderSide: BorderSide(
+                                width: 0,
+                                color:Colors.grey,
+                              ),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide(
+                                width: 0,
+                                color:Colors.grey,
+                              ),
+                            ),
+                            contentPadding: EdgeInsets.only(top: 12, left: 20),
+                            filled: true,
+                            fillColor: Colors.white24,
+                            border: InputBorder.none,
+
+                            hintText: "Enter your Nationality",
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(5),
+                              borderSide: BorderSide(
+                                width: 0,
+                                color:Colors.grey,
+                              ),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide(
+                                width: 0,
+                                color:Colors.grey,
+                              ),
+                            ),
+                            contentPadding: EdgeInsets.only(top: 12, left: 20),
+                            filled: true,
+                            fillColor: Colors.white24,
+                            border: InputBorder.none,
+
+                            hintText: "Enter your Age",
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 30),
+                  Container(
+                    height: 50,
+                    width: 100,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.grey[500],
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Submit",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
